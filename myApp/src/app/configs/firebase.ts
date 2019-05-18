83501772d3002c4dc3d7cd560660471afe646e11
WedsleyFernando <WedsleyFernando@users.noreply.github.com>
2019-05-18 18:06:27 -0300

    Remocao De Arquivos Nao Utilizados Mais

--- myApp/src/app/configs/firebase.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig ={
-    apiKey: "AIzaSyBe_ZEmRyQTifrUs4yPuhf_ZrG0G9SZmxI",
-    authDomain: "appvaloriza.firebaseapp.com",
-    databaseURL: "https://appvaloriza.firebaseio.com",
-    projectId: "appvaloriza",
-    storageBucket: "appvaloriza.appspot.com",
-    messagingSenderId: "236701307603"
-}
